3a2735f5d6301ad0bdaddb4ff74a8dcf632f3cd6
ali aziz <aa_connect@outlook.com>
2018-07-14 21:37:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,14 @@
 
 katz_deli = []
 
-def line(string)
-  puts "The line is currently empty."
+def line(deli)
+  if line == "there is nobody in line"
+    puts "The line is empty"
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << "#{i}. #{person}"
+    end
+    puts current_line
+  end
 end
