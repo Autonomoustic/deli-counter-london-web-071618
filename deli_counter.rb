5924881b300a71922ccfce2263c33ebe69c3dee4
ali aziz <aa_connect@outlook.com>
2018-07-14 21:01:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
-def line(katz_deli)
+
+katz_deli = []
+
+def line(string)
   puts "The line is currently empty."
 end
 
-def line(other_deli)
-  puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
-end
